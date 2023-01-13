73644c3d27f38b038c8802d19bdeb51036c92b76
ZManak <javifuer@gmail.com>
2023-01-13 14:11:11 +0100

    generalUpdates

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -24,12 +24,12 @@ async function getBooks(catName){
     document.querySelector(".canvasBooks").style.justifyContent = "center";
     printBooks(books, canvasBooks);
     document.querySelector(".bookCard").style.width = "75vw";
-    
+
 }
 
 function printCategories(categories, canvas) {
     let bookCategories = categories.results
-    
+
     for (let i = 0; i < bookCategories.length; i++){
         arrayCategories.push(bookCategories[i])
     }
@@ -37,7 +37,7 @@ function printCategories(categories, canvas) {
     for (let i = 0; i < arrayCategories.length; i++){
         let card = document.createElement("div")
         card.setAttribute("class", "catCard")
-        card.innerHTML = 
+        card.innerHTML =
             `<h3>${bookCategories[i].display_name}</h3>
             <p>Last published: ${bookCategories[i].newest_published_date}</p>
             <p>Oldest Published: ${bookCategories[i].oldest_published_date}</p>
@@ -49,7 +49,7 @@ function printCategories(categories, canvas) {
 
 function printBooks(books, canvas) {
     let bookList = books.results.books
-    
+
     for (let i = 0; i < bookList.length; i++){
         arrayBooks.push(bookList[i])
     }
@@ -57,7 +57,7 @@ function printBooks(books, canvas) {
     for (let i = 0; i < arrayBooks.length; i++){
         let card = document.createElement("div")
         card.setAttribute("class", "bookCard")
-        card.innerHTML = 
+        card.innerHTML =
             `<h3>#${arrayBooks[i].rank} ${arrayBooks[i].title}</h3>
             <img class="bookImg" src=${arrayBooks[i].book_image} alt=${bookList[i].title}>
             <p>Weeks on chart: ${arrayBooks[i].weeks_on_list}</p>
@@ -65,15 +65,15 @@ function printBooks(books, canvas) {
             <a href=${bookList[i].amazon_product_url} target="_blank"}>BUY</a>
             <button style = "button" id="addFav${i}">Add to Favorites</button>`
         canvas.appendChild(card);
-        const favBook = 
+        const favBook =
             { title: bookList[i].title,
             description: bookList[i].description,
             buy: bookList[i].amazon_product_url}
         const favButton = document.getElementById(`addFav${i}`)
         favButton.addEventListener('click', function() {
             addFav(firebase.auth().currentUser.uid, favBook)
-        })   
-            
+        })
+
     }
 }
 
@@ -81,7 +81,7 @@ function buttonEvent (){
         for (let i = 0; i < arrayCategories.length; i++) {
             let boton = document.querySelector(`.readMore${i}`);
             botones.push(boton);
-    
+
         }
         botones.forEach((element, i) => element.addEventListener("click", function (){
         getBooks(arrayCategories[i].list_name_encoded)}));
@@ -103,6 +103,6 @@ function addFav(userID, bookObject) {
                     doc.ref.update({ favs: doc.data().favs.concat(bookObject) })
                 }
         alert('Added to Favorites')
-        }) });    
+        }) });
     };
 
